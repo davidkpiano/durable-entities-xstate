cb89737d221bc504a13d3e34ae26ed2bf5501d17
David Khourshid <davidkpiano@gmail.com>
2020-10-12 11:16:43 -0400

    POC

---
+++ DonutTrigger/index.ts
@@ -0,0 +1,46 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import * as df from 'durable-functions';
+
+const httpTrigger: AzureFunction = async function(
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const { id } = req.query;
+
+  if (!id) {
+    context.res = {
+      status: 400,
+      body: 'Missing id',
+    };
+
+    return;
+  }
+
+  const client = df.getClient(context);
+  const entityId = new df.EntityId('DonutEntity', id);
+
+  switch (req.method) {
+    case 'POST': {
+      const eventObject = req.body;
+
+      await client.signalEntity(entityId, 'send', eventObject);
+
+      context.res = {
+        body: `Event "${eventObject.type}" sent to entity "${id}".`,
+      };
+
+      return;
+    }
+    case 'GET': {
+      const stateResponse = await client.readEntityState(entityId);
+
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify(stateResponse),
+      };
+      return;
+    }
+  }
+};
+
+export default httpTrigger;
